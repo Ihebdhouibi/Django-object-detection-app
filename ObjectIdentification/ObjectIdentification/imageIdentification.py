de14dca8c2a93df523cf96512f85a3041eb4523e
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2021-02-17 17:48:19 +0100

    implementing keras resnet50 algorithm

---
+++ ObjectIdentification/ObjectIdentification/imageIdentification.py
@@ -0,0 +1,20 @@
+import matplotlib
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.io import imread
+
+# Model instantiation
+model = ResNet50(weights='imagenet')
+
+img_path = r'C:\Users\ihebd\PycharmProjects\DjangoObjectDetectionApp\pug.jfif'
+
+# read our image
+img = imread(img_path)
+plt.interactive(True)
+print(img)
+
+plt.imshow(img)
+#n = input('give me a number rabk ! ')
