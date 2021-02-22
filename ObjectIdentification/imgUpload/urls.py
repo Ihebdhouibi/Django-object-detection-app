93c43383e5b2545a2b7504d37e413c022dc59365
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2021-02-22 15:50:52 +0100

    uploading image form is done

--- ObjectIdentification/imgUpload/urls.py
+++ ObjectIdentification/imgUpload/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'imageprocess', views.imageprocess, name='imageprocess')
 
 ]
--- ObjectIdentification/imgUpload/views.py
+++ ObjectIdentification/imgUpload/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
+from .form import ImageUploadForm
+
+
+def handle_uploaded_files(f):
+    with open('img.jpg', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 
+def imageprocess(request):
+    form = ImageUploadForm(request.POST, request.FILES)
+    if form.is_valid():
+        handle_uploaded_files(request.FILES['image'])
+    return render(request, 'result.html')
